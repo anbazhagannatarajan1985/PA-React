789a1e685514ac4ca6c69ec21eb3b1dd626c1fe0
Niloy Dey <niloybhusan.dey@fadv.com>
2020-05-23 13:29:51 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HelloWorldService } from "./Services/HelloWorldService";
 
 function App() {
+  const [greeting, setGreeting] = useState('Hi');
+
+  useEffect(() => {
+    setTimeout( () => {
+      setGreeting(HelloWorldService.getHelloWorld)
+    }, 5000);
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {greeting} World!!
         </p>
         <a
           className="App-link"
---
+++ src/Services/HelloWorldService.js
@@ -0,0 +1,7 @@
+export const HelloWorldService = {
+    getHelloWorld,
+}
+
+function getHelloWorld(){
+    return 'hello';
+}
